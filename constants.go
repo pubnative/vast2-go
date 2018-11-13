2431c941ae93aae80faa6ad389fe4b596b8c4cbf
Luis Vieira <luis.vieira@pubnative.net>
2018-11-13 18:11:25 +0100

    add mediafiles consts

--- constants.go
+++ constants.go
@@ -1,10 +1,16 @@
 package vast2
 
-// Tracking Events
-// VAST Version 2 Final Page 13, 14.
 // https://www.iab.com/wp-content/uploads/2015/11/VAST-2_0-FINAL.pdf
+
+// Could be present in CompanionAds, Linear, or NonLinearAds
 const (
 	TrackingEventCreativeView     = "creativeView"
+)
+
+// Tracking Events
+// Present in Linear, and NonLinearAds
+// VAST Version 2 Final Page 13, 14.
+const (
 	TrackingEventStart            = "start"
 	TrackingEventMindPoint        = "midpoint"
 	TrackingEventFirstQuartile    = "firstQuartile"
@@ -20,4 +26,13 @@ const (
 	TrackingEventCollapse         = "collapse"
 	TrackingEventAcceptInvitation = "acceptInvitation"
 	TrackingEventClose            = "close"
+)
+
+
+// MediaFilesDelivery
+// Present Only in Linear vasts
+// VAST Version 2 Final Page 11.
+const (
+	LinearMediaFileDeliveryStreaming = "streaming"
+	LinearMediaFileDeliveryProgressive = "progressive"
 )
