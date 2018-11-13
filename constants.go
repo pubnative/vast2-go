723741e3d39f6e14bb65c023a3280fb42d7f0392
Luis Vieira <luis.vieira@pubnative.net>
2018-11-13 18:03:00 +0100

    create a constanst file with tracking events

---
+++ constants.go
@@ -0,0 +1,23 @@
+package vast2
+
+// Tracking Events
+// VAST Version 2 Final Page 13, 14.
+// https://www.iab.com/wp-content/uploads/2015/11/VAST-2_0-FINAL.pdf
+const (
+	TrackingEventCreativeView     = "creativeView"
+	TrackingEventStart            = "start"
+	TrackingEventMindPoint        = "midpoint"
+	TrackingEventFirstQuartile    = "firstQuartile"
+	TrackingEventThirdQuartile    = "thirdQuartile"
+	TrackingEventComplete         = "complete"
+	TrackingEventMute             = "mute"
+	TrackingEventUnmute           = "unmute"
+	TrackingEventPause            = "pause"
+	TrackingEventRewing           = "rewind"
+	TrackingEventResume           = "resume"
+	TrackingEventFullscreen       = "fullscreen"
+	TrackingEventExpand           = "expand"
+	TrackingEventCollapse         = "collapse"
+	TrackingEventAcceptInvitation = "acceptInvitation"
+	TrackingEventClose            = "close"
+)
